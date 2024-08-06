b5e00a3892800790b3ef1245950ed22255c11aee
FreddyGF <fregiraldo1@misena.edu.co>
2024-08-06 09:00:42 -0500

    Parte de Freddy

---
+++ python/preguntas.py
@@ -0,0 +1,8 @@
+#Diccionario con preguntas y respuestas
+
+preguntas = {
+    "¿Cuál es la capital de Australia?": "canberra",
+    "¿Cuál es el rio mas largo del mundo?": "Nilo",
+    "¿En qué continente se encuentra el Monte Everest?": "Asia",
+    #Agregar mas Preguntas aqui
+}
