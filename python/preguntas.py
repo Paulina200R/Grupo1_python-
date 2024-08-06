b5671f3211d2c651c81cbed83e383ddec4914b63
Lisandrot1 <lmlisandro3@gmail.com>
2024-08-06 09:09:22 -0500

    melo mi parte

--- python/preguntas.py
+++ python/preguntas.py
@@ -1,1 +1,17 @@
 import random
+
+
+
+
+
+
+
+# parte lisandro
+for pregunta, respuesta in preguntas_aleatorias:
+    print(pregunta)
+    respuesta_usuario = input("Tu respuesta: ")
+    
+    if respuesta_usuario.lower() == respuesta.lower():
+        print("¡Correcto!")
+        puntaje += 1
+    else:
